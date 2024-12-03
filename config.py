c575fad64c06a59666d5e2818e3b218228e043d9
DilwarHosen <105662694+DilwarHosen@users.noreply.github.com>
2024-12-03 23:42:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/AloneXBot/AAROHIxMUSICv2",
+    "https://github.com/DilwarHosen/DilwarX",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
