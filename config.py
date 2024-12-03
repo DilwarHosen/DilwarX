f29eb4b9289df5e6f0a1c25c5c66f495d893a1b4
AlonePvtBot <132041403+AloneXBot@users.noreply.github.com>
2024-12-03 18:09:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ SONG_DOWNLOAD_DURATION = int(
 
 LOGGER_ID = int(getenv("LOGGER_ID", "-1001603822916"))
 
-OWNER_ID = int(getenv("OWNER_ID", "6079943111"))
+OWNER_ID = int(getenv("OWNER_ID", 6079943111))
 
 BOT_USERNAME = getenv("BOT_USERNAME" , "AloneXMusicBot")
 
